a64f442650174d0e96663bacbf55d508567a81b3
CreepyCatLeGoog <56730987+CreepyCatLeGoog@users.noreply.github.com>
2023-10-17 17:04:15 +0200

    feat master added some metrics

--- Extract/final.py
+++ Extract/final.py
@@ -1,32 +1,29 @@
 import requests
-import json
-from collections import defaultdict
-from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 import time
 
 crypto = {
     'Bitcoin': 'BTCUSDT',
-    'Ethereum': 'ETHUSDT',
-    'Binance Coin': 'BNBUSDT',
-    'Cardano': 'ADAUSDT',
-    'Solana': 'SOLUSDT',
-    'XRP': 'XRPUSDT',
-    'Polkadot': 'DOTUSDT',
-    'Dogecoin': 'DOGEUSDT',
-    'Avalanche': 'AVAXUSDT',
-    'Chainlink': 'LINKUSDT',
-    'Litecoin': 'LTCUSDT',
-    'Bitcoin Cash': 'BCHUSDT',
-    'Cosmos': 'ATOMUSDT',
-    'VeChain': 'VETUSDT',
-    'Filecoin': 'FILUSDT',
-    'Ethereum Classic': 'ETCUSDT',
-    'Tron': 'TRXUSDT',
-    'Stellar': 'XLMUSDT',
-    'Tezos': 'XTZUSDT',
-    'IOTA': 'IOTAUSDT',
-    'Neo': 'NEOUSDT'
+    # 'Ethereum': 'ETHUSDT',
+    # 'Binance Coin': 'BNBUSDT',
+    # 'Cardano': 'ADAUSDT',
+    # 'Solana': 'SOLUSDT',
+    # 'XRP': 'XRPUSDT',
+    # 'Polkadot': 'DOTUSDT',
+    # 'Dogecoin': 'DOGEUSDT',
+    # 'Avalanche': 'AVAXUSDT',
+    # 'Chainlink': 'LINKUSDT',
+    # 'Litecoin': 'LTCUSDT',
+    # 'Bitcoin Cash': 'BCHUSDT',
+    # 'Cosmos': 'ATOMUSDT',
+    # 'VeChain': 'VETUSDT',
+    # 'Filecoin': 'FILUSDT',
+    # 'Ethereum Classic': 'ETCUSDT',
+    # 'Tron': 'TRXUSDT',
+    # 'Stellar': 'XLMUSDT',
+    # 'Tezos': 'XTZUSDT',
+    # 'IOTA': 'IOTAUSDT',
+    # 'Neo': 'NEOUSDT'
     # Add more cryptocurrencies as needed
 }
 
@@ -40,7 +37,22 @@ def scrap_binance(symbol):
             weighted_avg_price = float(data['weightedAvgPrice'])
             volume = float(data['volume'])
             market_cap = weighted_avg_price * volume
+
             data['marketCap'] = market_cap
+
+            data['nvtRatio'] = market_cap / volume
+
+            funding_response = requests.get(f"https://fapi.binance.com/fapi/v1/fundingRate?symbol={symbol}")
+            funding_data = funding_response.json()
+            if funding_data:
+                last_funding_rate = funding_data[-1]['fundingRate']  # Get the last funding rate
+                data['lastFundingRate'] = last_funding_rate
+
+            open_interest_response = requests.get(f"https://fapi.binance.com/fapi/v1/openInterest?symbol={symbol}")
+            open_interest_data = open_interest_response.json()
+            if open_interest_data:
+                last_open_interest = open_interest_data['openInterest']  # Get the last open interest
+                data['lastOpenInterest'] = last_open_interest
 
         for key, value in data.items():
             print(f"{key}: {value}")
@@ -54,6 +66,7 @@ def main(crypto):
         futures = []
         for symbol in crypto.values():
             futures.append(executor.submit(scrap_binance, symbol))
+            # futures.append(executor.submit(get_historical_data, symbol, "1d"))
         
         for future in futures:
             future.result()
