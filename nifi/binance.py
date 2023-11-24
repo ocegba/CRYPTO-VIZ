342b89523ba0f8bd3eefd6dc7405ef99c675eac3
ocegba <oceane.gbamene@epitech.eu>
2023-11-24 12:06:26 +0100

    binance websocket

--- nifi/binance.py
+++ nifi/binance.py
@@ -1,34 +1,12 @@
-import websocket
-import json
-import requests
+import asyncio
+import websockets
 
-# Fonction pour envoyer des données à NiFi
-def send_to_nifi(data):
-    nifi_endpoint = 'https://localhost:8443/nifi/'  # Remplacez par votre endpoint NiFi
-    headers = {'Content-Type': 'application/json'}
-    payload = {'your_key': data}  # Adapter la structure de données selon vos besoins
-    response = requests.post(nifi_endpoint, headers=headers, data=json.dumps(payload))
-    print(response.text)
+async def produce(message: str, host:str, port:int):
+    async with websockets.connect(f"ws://{host}:{port}/binance") as ws:
+        print(message)
+        await ws.send(message)
+        print("=============================================")
+        await ws.recv()
 
-def on_message(ws, message):
-    formatted_data = json.loads(message)
-    
-    send_to_nifi(formatted_data)
-
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws):
-    print("Connection closed")
-
-def on_open(ws):
-    print("Connection opened")
-
-if __name__ == "__main__":
-    ws_endpoint = "'wss://stream.binance.com:9443/ws/solusdt@kline_1m"  # Remplacez par l'URL du flux WebSocket Binance
-    ws = websocket.WebSocketApp(ws_endpoint,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(produce(message='{"msg: "lol"}', host="nifi", port=6890))
