be3f87d0afe97cf613dd6eba45dd15802e810782
Gautier Pamies <gautier.pamies34@gmail.com>
2023-12-08 14:27:40 +0100

    docker_nifi

--- nifi/binance_websocket.py
+++ nifi/binance_websocket.py
@@ -8,41 +8,46 @@ async def on_message(message):
     print(message)
 
 async def on_error(error):
-    print(error)
+    print("error", error)
 
 async def on_close(close_msg):
     print("### closed ###" + close_msg)
 
 async def streamKline(currency, interval, nifi_host, nifi_port):
+
     uri = f'wss://stream.binance.com:9443/ws/{currency}@kline_{interval}'
-    async with websockets.connect(uri) as ws:
+    async with websockets.connect(uri) as ws_nifi:
+        print("hello", uri)
         while True:
             try:
-                message = await ws.recv()
+                message = await ws_nifi.recv()
                 await on_message(message)
-
+                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++", nifi_host, nifi_port)
                 async with websockets.connect(f"ws://{nifi_host}:{nifi_port}/binance") as ws_nifi: #ListenWebSocket 
-                    await ws_nifi.send(message)
+                    print("--------------------------------------------------------------------------------------------------------------------------", nifi_host, nifi_port)
+                    await ws_nifi.send("hello", message)
                     print("Sent to NiFi")
 
             except websockets.exceptions.ConnectionClosedError as e:
                 await on_close(str(e))
+                print('on close')
                 break
             except Exception as e:
                 await on_error(str(e))
+                print('on error')
 
-async def produce(message: str, host:str, port:int):
+async def produce(message: str, host:str, port:str):
     async with websockets.connect(f"ws://{host}:{port}/binance") as ws:
         print(message)
         await ws.send(message)
-        print("=============================================")
+        print("===================================================================================================================================")
         await ws.recv()
 
 async def main():
     currency = 'btcusdt'
     interval = '1m'
     nifi_host = "nifi"
-    nifi_port = 6890
+    nifi_port = 6993
 
     await streamKline(currency, interval, nifi_host, nifi_port)
 
