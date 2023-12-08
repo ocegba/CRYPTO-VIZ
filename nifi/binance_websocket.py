72ab8bb416b2b00809d145b3d0b5f73dd6f16323
ocegba <oceane.gbamene@epitech.eu>
2023-12-08 16:11:21 +0100

    update binance websocket

--- nifi/binance_websocket.py
+++ nifi/binance_websocket.py
@@ -16,33 +16,22 @@ async def on_close(close_msg):
 async def streamKline(currency, interval, nifi_host, nifi_port):
 
     uri = f'wss://stream.binance.com:9443/ws/{currency}@kline_{interval}'
-    async with websockets.connect(uri) as ws_nifi:
-        print("hello", uri)
+    async with websockets.connect(uri) as ws_binance:
         while True:
             try:
-                message = await ws_nifi.recv()
+                message = await ws_binance.recv()
                 await on_message(message)
-                print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++", nifi_host, nifi_port)
+
                 async with websockets.connect(f"ws://{nifi_host}:{nifi_port}/binance") as ws_nifi: #ListenWebSocket 
-                    print("--------------------------------------------------------------------------------------------------------------------------", nifi_host, nifi_port)
-                    await ws_nifi.send("hello", message)
+                    await ws_nifi.send(message)
                     print("Sent to NiFi")
 
             except websockets.exceptions.ConnectionClosedError as e:
                 await on_close(str(e))
-                print('on close')
                 break
-            except Exception as e:
-                print(nifi_host, "nifi_host")
-                await on_error(str(e))
-                print('on error')
 
-async def produce(message: str, host:str, port:str):
-    async with websockets.connect(f"ws://{host}:{port}/binance") as ws:
-        print(message)
-        await ws.send(message)
-        print("===================================================================================================================================")
-        await ws.recv()
+            except Exception as e:
+                await on_error(str(e))
 
 async def main():
     currency = 'btcusdt'
