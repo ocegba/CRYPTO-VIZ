69a6aed03cacd1f89bdb0e7ddd4987fcecbb482a
Rafik Merzouk <rafikmerzouk@Rafiks-MacBook-Pro.local>
2024-01-25 10:37:16 +0100

    adding: kafka connect not working

---
+++ kafka-connect/init-kafka-connect.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A brief pause to ensure Kafka Connect is fully operational
+sleep 10
+
+echo "Submitting connector configuration to Kafka Connect"c
+curl -X POST -H "Content-Type: application/json" --data @./config/elasticsearch-sink.json http://localhost:8083/connectors
+echo "Connector configuration submission attempted"
